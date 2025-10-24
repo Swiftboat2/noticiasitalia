41f5ad1ea8d0b88108dd21cfa13955dd55cb9d32
lautaro pelozo <lautaropelozo@gmail.com>
2025-10-24 14:21:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8470533760-cc0e2",
+  "appId": "1:173589929744:web:35bf8be7110eab55d60f95",
+  "apiKey": "AIzaSyBlaCGMy87AAfSXIEDOu-uHHo1imWWqdbY",
+  "authDomain": "studio-8470533760-cc0e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "173589929744"
+};
